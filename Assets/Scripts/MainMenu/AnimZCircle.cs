aeb6f8bfdefc27698893051ac751a891b6ecca81
Marc Alcón Meliá <marcaalconmelia@gmail.com>
2022-04-06 00:33:57 +0200

    Make zodiac circle animations for the main menu

--- Assets/Scripts/MainMenu/AnimZCircle.cs
+++ Assets/Scripts/MainMenu/AnimZCircle.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimGameTitle : MonoBehaviour
+public class AnimZCircle : MonoBehaviour
 {
     private Animator gtitleAnim;
 
