d3cb962edc6e3ad48f8b9a92b7a764d0758258ac
Marc Alcón Meliá <marcaalconmelia@gmail.com>
2022-04-05 22:41:02 +0200

    Make game title appear animation

---
+++ Assets/Scripts/MainMenu/AnimGameTitle.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimGameTitle : MonoBehaviour
+{
+    private Animator gtitleAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gtitleAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
