dcdd95d5d3287a5448c7e8de73e7a0476df38dd7
Marc Alcón Meliá <marcaalconmelia@gmail.com>
2022-04-20 19:40:08 +0200

    Add attack button interaction

--- Assets/Scripts/BattleSys/BattleManager.cs
+++ Assets/Scripts/BattleSys/BattleManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum gameStates { choosing, attacking, acting }
 
 public class BattleManager : MonoBehaviour
 {
@@ -8,11 +11,26 @@ public class BattleManager : MonoBehaviour
     private int amountSpawn;
     public Transform leftLimitSpawn;
     public Transform rightLimitSpawn;
+
     public GameObject parentEnemies;
+    public List<GameObject> enemiesSpawned;
+
+    [HideInInspector] public gameStates state = gameStates.choosing;
+
+    public GameObject normalText;
+    [HideInInspector] public string baseText;
 
     private void Start()
     {
         amountSpawn = Random.Range(1,5);
+        if (amountSpawn == 1)
+            normalText.GetComponent<Text>().text = "    YOU ARE UP AGAINST " + amountSpawn + " ENEMY";
+        else
+            normalText.GetComponent<Text>().text = "    YOU ARE UP AGAINST " + amountSpawn + " ENEMIES";
+
+        baseText = normalText.GetComponent<Text>().text;
+        enemiesSpawned = new List<GameObject>();
+
         SpawnCards();
     }
 
@@ -23,7 +41,7 @@ public class BattleManager : MonoBehaviour
         {
             int numberCard = Random.Range(0, enemyCards.Count);
             Vector3 halfScreenPosition = new Vector3(leftLimitSpawn.position.x + (Mathf.Abs(rightLimitSpawn.position.x) + Mathf.Abs(leftLimitSpawn.position.x)) / 2, rightLimitSpawn.position.y, rightLimitSpawn.position.z);
-            Instantiate(enemyCards[numberCard], halfScreenPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform);
+            enemiesSpawned.Add(Instantiate(enemyCards[numberCard], halfScreenPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform));
         }
 
         // Two cards spawn
@@ -33,11 +51,11 @@ public class BattleManager : MonoBehaviour
             Vector3 halfScreenPosition = new Vector3(leftLimitSpawn.position.x + (Mathf.Abs(rightLimitSpawn.position.x) + Mathf.Abs(leftLimitSpawn.position.x)) / 2, rightLimitSpawn.position.y, rightLimitSpawn.position.z);
             
             Vector3 spawnPosition = new Vector3(leftLimitSpawn.position.x + (Mathf.Abs(leftLimitSpawn.position.x) + Mathf.Abs(halfScreenPosition.x)) / 2 + 1, rightLimitSpawn.position.y, rightLimitSpawn.position.z);
-            Instantiate(enemyCards[numberCard], spawnPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform);
+            enemiesSpawned.Add(Instantiate(enemyCards[numberCard], spawnPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform));
 
             numberCard = Random.Range(0, enemyCards.Count);
             spawnPosition = new Vector3(halfScreenPosition.x + (Mathf.Abs(leftLimitSpawn.position.x) + Mathf.Abs(halfScreenPosition.x)) / 2 - 1, rightLimitSpawn.position.y, rightLimitSpawn.position.z);
-            Instantiate(enemyCards[numberCard], spawnPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform);
+            enemiesSpawned.Add(Instantiate(enemyCards[numberCard], spawnPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform));
         }
 
         // Three cards spawn
@@ -47,14 +65,14 @@ public class BattleManager : MonoBehaviour
             Vector3 halfScreenPosition = new Vector3(leftLimitSpawn.position.x + (Mathf.Abs(rightLimitSpawn.position.x) + Mathf.Abs(leftLimitSpawn.position.x)) / 2, rightLimitSpawn.position.y, rightLimitSpawn.position.z);
             
             Vector3 spawnPosition = new Vector3(leftLimitSpawn.position.x + (Mathf.Abs(leftLimitSpawn.position.x) + Mathf.Abs(halfScreenPosition.x)) / 2, rightLimitSpawn.position.y, rightLimitSpawn.position.z);
-            Instantiate(enemyCards[numberCard], spawnPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform);
+            enemiesSpawned.Add(Instantiate(enemyCards[numberCard], spawnPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform));
 
             numberCard = Random.Range(0, enemyCards.Count);
-            Instantiate(enemyCards[numberCard], halfScreenPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform);
+            enemiesSpawned.Add(Instantiate(enemyCards[numberCard], halfScreenPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform));
 
             numberCard = Random.Range(0, enemyCards.Count);
             spawnPosition = new Vector3(halfScreenPosition.x + (Mathf.Abs(leftLimitSpawn.position.x) + Mathf.Abs(halfScreenPosition.x)) / 2, rightLimitSpawn.position.y, rightLimitSpawn.position.z);
-            Instantiate(enemyCards[numberCard], spawnPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform);
+            enemiesSpawned.Add(Instantiate(enemyCards[numberCard], spawnPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform));
         }
 
         // Four cards spawn
@@ -64,19 +82,19 @@ public class BattleManager : MonoBehaviour
             Vector3 halfScreenPosition = new Vector3(leftLimitSpawn.position.x + (Mathf.Abs(rightLimitSpawn.position.x) + Mathf.Abs(leftLimitSpawn.position.x)) / 4, rightLimitSpawn.position.y, rightLimitSpawn.position.z);
             Vector3 halfhalfScreenPosition = new Vector3(leftLimitSpawn.position.x + (Mathf.Abs(rightLimitSpawn.position.x) + Mathf.Abs(leftLimitSpawn.position.x)) / 8, rightLimitSpawn.position.y, rightLimitSpawn.position.z);
 
-            Instantiate(enemyCards[numberCard], halfhalfScreenPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform);
+            enemiesSpawned.Add(Instantiate(enemyCards[numberCard], halfhalfScreenPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform));
 
             numberCard = Random.Range(0, enemyCards.Count);
             Vector3 spawnPosition = new Vector3(halfScreenPosition.x - halfhalfScreenPosition.x, halfhalfScreenPosition.y, halfhalfScreenPosition.z);
-            Instantiate(enemyCards[numberCard], spawnPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform);
+            enemiesSpawned.Add(Instantiate(enemyCards[numberCard], spawnPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform));
 
             numberCard = Random.Range(0, enemyCards.Count);
             spawnPosition = new Vector3(leftLimitSpawn.position.x - halfhalfScreenPosition.x, halfhalfScreenPosition.y, halfhalfScreenPosition.z);
-            Instantiate(enemyCards[numberCard], spawnPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform);
+            enemiesSpawned.Add(Instantiate(enemyCards[numberCard], spawnPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform));
 
             numberCard = Random.Range(0, enemyCards.Count);
             spawnPosition = new Vector3(-halfhalfScreenPosition.x, halfhalfScreenPosition.y, halfhalfScreenPosition.z);
-            Instantiate(enemyCards[numberCard], spawnPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform);
+            enemiesSpawned.Add(Instantiate(enemyCards[numberCard], spawnPosition, enemyCards[numberCard].transform.rotation, parentEnemies.transform));
         }
     }
 }
--- Assets/Scripts/BattleSys/ButtonManager.cs
+++ Assets/Scripts/BattleSys/ButtonManager.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
     public List<GameObject> battleButtons;
+    public BattleManager battleManager;
 
-    private bool pressed = false;
+    private bool pressedHor = false;
+    private bool pressedVer = false;
     private int currentButtonIndex;
+    private int currentTextIndex;
+
+    public List<GameObject> enemyTexts;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +25,29 @@ public class ButtonManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Go back
+        if (Input.GetKey(KeyCode.X) && battleManager.state != gameStates.choosing)
+        {
+            battleManager.state = gameStates.choosing;
+            battleManager.normalText.GetComponent<Text>().text = battleManager.baseText;
+
+            // Disable enemy texts
+            enemyTexts[currentTextIndex].GetComponent<Text>().enabled = true;
+            enemyTexts[currentTextIndex].transform.GetChild(0).gameObject.SetActive(false);
+            enemyTexts[currentTextIndex].transform.GetChild(1).gameObject.SetActive(false);
+
+            foreach (GameObject text in enemyTexts)
+                text.SetActive(false);
+
+            battleManager.normalText.SetActive(true);
+        }
+
+        // Change choosing button
         float leftRight = Input.GetAxis("Horizontal");
 
-        if (leftRight < 0 && !pressed)
+        if (leftRight < 0 && !pressedHor && battleManager.state == gameStates.choosing)
         {
-            pressed = true;
+            pressedHor = true;
             if (currentButtonIndex == 0)
             {
                 battleButtons[currentButtonIndex].GetComponent<PressBattleButton>().OnExitSelection();
@@ -39,9 +63,9 @@ public class ButtonManager : MonoBehaviour
             }
         }
 
-        else if (leftRight > 0 && !pressed)
+        else if (leftRight > 0 && !pressedHor && battleManager.state == gameStates.choosing)
         {
-            pressed = true;
+            pressedHor = true;
             if (currentButtonIndex == battleButtons.Count - 1)
             {
                 battleButtons[currentButtonIndex].GetComponent<PressBattleButton>().OnExitSelection();
@@ -57,9 +81,94 @@ public class ButtonManager : MonoBehaviour
             }
         }
 
-        else if (leftRight == 0 && pressed)
+        else if (leftRight == 0 && pressedHor && battleManager.state == gameStates.choosing)
         {
-            pressed = false;
+            pressedHor = false;
+        }
+
+        // Attack
+        if (currentButtonIndex == 0 && Input.GetKey(KeyCode.Z) && battleManager.state != gameStates.attacking)
+        {
+            battleManager.state = gameStates.attacking;
+            battleManager.normalText.SetActive(false);
+
+            for (int i = 0; i < battleManager.enemiesSpawned.Count; i++)
+            {
+                enemyTexts[i].SetActive(true);
+                enemyTexts[i].GetComponent<Text>().text = battleManager.enemiesSpawned[i].name;
+                enemyTexts[i].transform.GetChild(1).GetComponent<Text>().text = "    " + battleManager.enemiesSpawned[i].name;
+            }
+
+            currentTextIndex = 0;
+            enemyTexts[currentButtonIndex].GetComponent<Text>().enabled = false;
+            enemyTexts[currentButtonIndex].transform.GetChild(0).gameObject.SetActive(true);
+            enemyTexts[currentButtonIndex].transform.GetChild(1).gameObject.SetActive(true);
+        }
+
+        float topBottom = Input.GetAxis("Vertical");
+        if (topBottom > 0 && !pressedVer && battleManager.state == gameStates.attacking)
+        {
+            pressedVer = true;
+            if (currentTextIndex == 0)
+            {
+                enemyTexts[currentTextIndex].GetComponent<Text>().enabled = true;
+                enemyTexts[currentTextIndex].transform.GetChild(0).gameObject.SetActive(false);
+                enemyTexts[currentTextIndex].transform.GetChild(1).gameObject.SetActive(false);
+
+                currentTextIndex = battleManager.enemiesSpawned.Count - 1;
+
+                enemyTexts[currentTextIndex].GetComponent<Text>().enabled = false;
+                enemyTexts[currentTextIndex].transform.GetChild(0).gameObject.SetActive(true);
+                enemyTexts[currentTextIndex].transform.GetChild(1).gameObject.SetActive(true);
+            }
+
+            else
+            {
+                enemyTexts[currentTextIndex].GetComponent<Text>().enabled = true;
+                enemyTexts[currentTextIndex].transform.GetChild(0).gameObject.SetActive(false);
+                enemyTexts[currentTextIndex].transform.GetChild(1).gameObject.SetActive(false);
+
+                currentTextIndex--;
+
+                enemyTexts[currentTextIndex].GetComponent<Text>().enabled = false;
+                enemyTexts[currentTextIndex].transform.GetChild(0).gameObject.SetActive(true);
+                enemyTexts[currentTextIndex].transform.GetChild(1).gameObject.SetActive(true);
+            }
+        }
+
+        else if (topBottom < 0 && !pressedVer && battleManager.state == gameStates.attacking)
+        {
+            pressedVer = true;
+            if (currentTextIndex == battleManager.enemiesSpawned.Count - 1)
+            {
+                enemyTexts[currentTextIndex].GetComponent<Text>().enabled = true;
+                enemyTexts[currentTextIndex].transform.GetChild(0).gameObject.SetActive(false);
+                enemyTexts[currentTextIndex].transform.GetChild(1).gameObject.SetActive(false);
+
+                currentTextIndex = 0;
+
+                enemyTexts[currentTextIndex].GetComponent<Text>().enabled = false;
+                enemyTexts[currentTextIndex].transform.GetChild(0).gameObject.SetActive(true);
+                enemyTexts[currentTextIndex].transform.GetChild(1).gameObject.SetActive(true);
+            }
+
+            else
+            {
+                enemyTexts[currentTextIndex].GetComponent<Text>().enabled = true;
+                enemyTexts[currentTextIndex].transform.GetChild(0).gameObject.SetActive(false);
+                enemyTexts[currentTextIndex].transform.GetChild(1).gameObject.SetActive(false);
+
+                currentTextIndex++;
+
+                enemyTexts[currentTextIndex].GetComponent<Text>().enabled = false;
+                enemyTexts[currentTextIndex].transform.GetChild(0).gameObject.SetActive(true);
+                enemyTexts[currentTextIndex].transform.GetChild(1).gameObject.SetActive(true);
+            }
+        }
+
+        else if (topBottom == 0 && pressedVer)
+        {
+            pressedVer = false;
         }
     }
 }
