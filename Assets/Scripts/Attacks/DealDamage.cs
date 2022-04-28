91ce1e0f5cb0341e43af3d88da83a3a31c1dd29d
Marc Alcón Meliá <marcaalconmelia@gmail.com>
2022-04-28 11:42:55 +0200

    Add enemy attack two 2.0 and three 2.0

--- Assets/Scripts/Attacks/DealDamage.cs
+++ Assets/Scripts/Attacks/DealDamage.cs
@@ -11,6 +11,14 @@ public class DealDamage : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             GameMaster.playerLife -= 2;
+            collision.gameObject.GetComponent<AudioSource>().Play();
+            collision.gameObject.GetComponent<Animator>().SetBool("Hit", true);
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            collision.gameObject.GetComponent<Animator>().SetBool("Hit", false);
     }
 }
